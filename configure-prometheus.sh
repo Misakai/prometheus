2f628493bf4e18479c2cc920344c738073f8c4ec
Roman Atachiants <roman.atachiants@gmail.com>
2015-11-25 20:39:37 +0100

    Update configure-prometheus.sh

--- configure-prometheus.sh
+++ configure-prometheus.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
+mkdir -p /var/monitors/
 COUNT=$(wget -q -O -"$@" http://rancher-metadata/2015-07-25/hosts | tail -1 | sed 's/=.*//')
-for (( c=1; c <= COUNT; c++))
+for c in $(seq 0 $COUNT)
 do
 	APP=$(wget -q -O -"$@" http://rancher-metadata/2015-07-25/hosts/$c/labels/app)
-	if [ "$APP" -eq "emitter" ]
+	if [ "$APP" = "emitter" ]
 	then
 		target=$(wget -q -O -"$@" http://rancher-metadata/2015-07-25/hosts/$c/agent_ip)
 		echo "- targets:" > /var/monitors/monitor${c}.yaml
